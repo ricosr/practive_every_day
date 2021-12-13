fd209f68febd910ed74acfbf85f82b3a2536f978
rico sun <ricosr@163.com>
2021-12-14 00:52:05 +0800

    学习notify wait:continue2

--- java/studyDemos/src/main/java/com/sunrui/thread/TestWaitMotify.java
+++ java/studyDemos/src/main/java/com/sunrui/thread/TestWaitMotify.java
@@ -1,6 +1,7 @@
 package com.sunrui.thread;
 
 
+
 class MonitorObject {
     public static int num = 4;
 
@@ -24,23 +25,27 @@ class MyWaitNotify3{
 
     boolean wasSignalled = false;
 
-    public void doWait(){
+    public void doWait(String threadName){
         synchronized(myMonitorObject){
+            System.out.println(threadName + " 1--------------->" + wasSignalled);
             while(!wasSignalled){
                 try{
-                    this.myMonitorObject.cusNum();
+                    System.out.println(threadName + " 11111111111111111111111111");
                     myMonitorObject.wait();
                 } catch(InterruptedException e){
                     e.printStackTrace();
                 }
+                System.out.println(threadName + " 22222222222222222222222222");
             }
+            this.myMonitorObject.cusNum();
             //clear signal and continue running.
             wasSignalled = false;
         }
     }
 
-    public void doNotify(){
+    public void doNotify(String threadName){
         synchronized(myMonitorObject){
+            System.out.println(threadName + " 2--------------->" + wasSignalled);
             this.myMonitorObject.proNum();
             wasSignalled = true;
             myMonitorObject.notify();
@@ -56,7 +61,14 @@ class TestRunPro implements Runnable{
 
     @Override
     public void run() {
-        this.myWaitNotify3.doNotify();
+        while (true) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            this.myWaitNotify3.doNotify(Thread.currentThread().getName());
+        }
     }
 }
 
@@ -68,7 +80,14 @@ class TestRunCus implements Runnable{
 
     @Override
     public void run() {
-        this.myWaitNotify3.doWait();
+        while (true) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            this.myWaitNotify3.doWait(Thread.currentThread().getName());
+        }
     }
 }
 
@@ -77,11 +96,54 @@ class WaitNotify {
     public static void main(String args[]) {
         MonitorObject myMonitor = new MonitorObject();
         MyWaitNotify3 myWaitNotify3 = new MyWaitNotify3(myMonitor);
+//        new Thread(new TestRunCus(myWaitNotify3)).start();
+        new Thread(new TestRunCus(myWaitNotify3)).start();
         new Thread(new TestRunCus(myWaitNotify3)).start();
         new Thread(new TestRunCus(myWaitNotify3)).start();
         new Thread(new TestRunPro(myWaitNotify3)).start();
-        new Thread(new TestRunPro(myWaitNotify3)).start();
+//        new Thread(new TestRunPro(myWaitNotify3)).start();
+//        new Thread(new TestRunCus(myWaitNotify3)).start();
     }
 }
+
+
+/*
+
+可以清楚看出每次做reduce的不是同一个线程，即打印出22222222222222222222222222的，即这个线程被notify唤醒
+
+Thread-3 2--------------->false
+add-----------------> 5
+Thread-1 1--------------->true
+reduce-----------------> 4
+Thread-0 1--------------->false
+Thread-0 11111111111111111111111111
+Thread-2 1--------------->false
+Thread-2 11111111111111111111111111
+Thread-1 1--------------->false
+Thread-1 11111111111111111111111111
+Thread-3 2--------------->false
+add-----------------> 5
+Thread-0 22222222222222222222222222
+reduce-----------------> 4
+Thread-0 1--------------->false
+Thread-0 11111111111111111111111111
+Thread-3 2--------------->false
+add-----------------> 5
+Thread-2 22222222222222222222222222
+reduce-----------------> 4
+Thread-3 2--------------->false
+add-----------------> 5
+Thread-1 22222222222222222222222222
+reduce-----------------> 4
+Thread-2 1--------------->false
+Thread-2 11111111111111111111111111
+Thread-1 1--------------->false
+Thread-1 11111111111111111111111111
+Thread-3 2--------------->false
+add-----------------> 5
+Thread-0 22222222222222222222222222
+reduce-----------------> 4
+
+ */
 
 
